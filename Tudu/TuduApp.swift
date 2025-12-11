73b0c4823372ac111fcd7beab8931a8a960250b4
AslamSDM <aslamprpd@gmail.com>
2025-12-11 13:20:34 +0530

    Initial Commit

---
+++ Tudu/TuduApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuduApp.swift
+//  Tudu
+//
+//  Created by Aslam on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TuduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
